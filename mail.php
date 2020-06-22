28f95911ea8424b862a356847697d4f9f79bba0b
SouadHenane <57634566+SouadHenane@users.noreply.github.com>
2020-06-22 06:22:48 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -37,26 +37,11 @@ if(get_magic_quotes_gpc()) {
 	$comments = stripslashes($comments);
 }
 
+$address = "souad.henane95@gmail.com";
 
-// Configuration option.
-// Enter the email address that you want to emails to be sent to.
-// Example $address = "joe.doe@yourdomain.com";
-
-//$address = "example@themeforest.net";
-$address = "example@yourdomain.com";
-
-
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
-
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
 
 $e_subject = 'You\'ve been contacted by ' . $form_name . '.';
 
-
-// Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
 $e_body = "You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project." . PHP_EOL . PHP_EOL;
 $e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
